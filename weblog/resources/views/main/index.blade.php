07c168d557d009aea66615ad735bafb16fce06f4
Khalid Obaide <sayedkhalidobaide@gmail.com>
2019-12-23 13:33:39 +0800

    Added Index Page

--- weblog/resources/views/main/index.blade.php
+++ weblog/resources/views/main/index.blade.php
@@ -1,8 +1,76 @@
 @extends('base')
 @section('title') Home @endsection 
 @section('body')
+<br><br><br>
 
-<h1> Who Am I ? </h1>
 
+<!-- Who Am I -->
+<div class="container">
+	<h3> Who Am I ? </h3>
+	<div class="card bg-dark" style="color: white;">
+		<p style="margin : 10px;">
+		Hello I am Khalid Obaide. The Owner Of this weblog. <br>
+		I really thank you for comming in and enjoying my artices go ahead and leave a comment if you think i am awsome if not just go ahead and again drop a like or comment below. <br>
+
+		Thanks Guys I really love to post more and more things but there is no such time for me to do that so ... 
+		<br>
+		And Again Khalid Obaide the Owner Of this Weblog  
+		</p>
+	</div>
+</div>
+
+
+
+<!-- Best Of ME -->
+<br><br><br>
+<div class="container">
+	<h3>Best Of Me :</h3>
+
+	<br>
+	<div class="card bg-success" style="margin-bottom : 20px;">
+		<h4 style="color : white; margin : 10px;">About The World ?</h4>
+		<a href="#"class="btn btn-info" style="width : 120px; position : absolute; right : 6px; top: 6px; color : white;">Read Now</a>
+	</div>	
+
+	<div class="card bg-success" style="margin-bottom : 20px;">
+		<h4 style="color : white; margin : 10px;">Why Islam?</h4>
+		<a href="#"class="btn btn-info" style="width : 120px; position : absolute; right : 6px; top: 6px; color : white;">Read Now</a>
+	</div>
+
+
+</div>
+
+
+<br><br><br>
+<!-- Contact Me -->
+<div class="container">
+	<h3>Contact Me : </h3>
+	<br>
+
+<div class="containerr">
+  <form action="/action_page.php">
+    <label for="fname">First Name</label>
+    <input class="sm-ipt" type="text" id="fname" name="name" placeholder="Your name..">
+
+    <label for="lname">Email</label>
+    <input type="email" class="sm-ipt" id="lname" name="email" placeholder="Your Email..">
+
+    <label for="subject">Subject</label>
+    <textarea id="subject" class="sm-ipt" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+
+<br><br>
+    <input type="submit" value="Submit" class="cta-btn">
+  </form>
+</div>
+
+	
+</div>
+
+<br><br><br><br>
+
+
+<!-- Copyright reserved -->
+<center>Made By <b>Khalid Obaide</b></center>
 
 @endsection 
--- weblog/resources/views/style.blade.php
+++ weblog/resources/views/style.blade.php
@@ -9,5 +9,39 @@
 	height : 300px;
 	margin-left : 150px;
 }
+
+
+
+.sm-ipt, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+.cta-btn {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.cta-btn:hover {
+  background-color: #45a049;
+}
+
+.containerr {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+
+
 </style>
 @yield('rest')
